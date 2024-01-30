dc613c4499e3e92e7460f2269879cad3bb383e90
Tkanno <38493521+yodaka0@users.noreply.github.com>
2024-01-30 15:18:55 +0900

    Add files via upload

---
+++ make_batch.py
@@ -0,0 +1,20 @@
+import os
+
+bat_contents = '''
+@echo on
+(
+cd {}
+
+conda activate pytorch-wildlife
+
+python exec_mdet.py session_root=%~dp0 threshold=0.2
+
+conda deactivate
+
+pause
+)
+'''.format(os.getcwd())
+
+# ファイルを書き込みモードで開く
+with open('detect.bat', 'w') as file:
+    file.write(bat_contents) 
