a0f230425505c36f0aaf2f7acc19ab1acbed1283
Tkanno <38493521+yodaka0@users.noreply.github.com>
2024-07-23 15:51:14 +0900

    Update make_batch_gui.py

--- make_batch_gui.py
+++ make_batch_gui.py
@@ -1,4 +1,7 @@
 import os
+import subprocess
+
+current_dir = os.getcwd()
 
 bat_contents = '''
 @echo on
@@ -13,8 +16,34 @@ conda deactivate
 
 pause
 )
-'''.format(os.getcwd())
+'''.format(current_dir)
 
 # ファイルを書き込みモードで開く
 with open('detect_gui.bat', 'w') as file:
     file.write(bat_contents) 
+
+
+def create_shortcuts_in_directory(directory):
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    shortcut_path = os.path.join(desktop, "detect_gui.lnk")
+    target_path = os.path.join(directory, "detect_gui.bat")
+
+    vbs_script = f"""
+Set WshShell = WScript.CreateObject("WScript.Shell")
+Set Shortcut = WshShell.CreateShortcut("{shortcut_path}")
+Shortcut.TargetPath = "{target_path}"
+Shortcut.WorkingDirectory = "{os.path.dirname(target_path)}"
+Shortcut.WindowStyle = 1
+Shortcut.IconLocation = "{target_path}, 0"
+Shortcut.Save
+    """
+    with open("create_shortcut.vbs", "w") as file:
+        file.write(vbs_script)
+
+    subprocess.run(['cscript', '//nologo', 'create_shortcut.vbs'])
+    os.remove("create_shortcut.vbs")
+
+
+if __name__ == "__main__":
+    create_shortcuts_in_directory(current_dir)
+    print("Shortcuts created successfully on the desktop.")
