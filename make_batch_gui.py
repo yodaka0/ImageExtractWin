2efcaa4d9b7dc75cb4eb4c6c31cddf5b3888aa6d
Tkanno <38493521+yodaka0@users.noreply.github.com>
2024-07-24 15:36:13 +0900

    Update make_batch_gui.py

--- make_batch_gui.py
+++ make_batch_gui.py
@@ -3,47 +3,44 @@ import subprocess
 
 current_dir = os.getcwd()
 
-bat_contents = '''
-@echo on
-(
+sh_contents = '''
+#!/bin/bash
 cd {}
 
-conda activate pwlife
+source activate pwlife
 
 python mdet_gui.py
 
-conda deactivate
-
-pause
-)
+source deactivate
 '''.format(current_dir)
 
 # ファイルを書き込みモードで開く
-with open('detect_gui.bat', 'w') as file:
-    file.write(bat_contents) 
+with open('detect_gui.sh', 'w') as file:
+    file.write(sh_contents)
 
+# 実行権限を付与する
+os.chmod('detect_gui.sh', 0o755)
 
 def create_shortcuts_in_directory(directory):
-    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-    shortcut_path = os.path.join(desktop, "detect_gui.lnk")
-    target_path = os.path.join(directory, "detect_gui.bat")
+    desktop = os.path.join(os.path.expanduser('~'), 'Desktop')
+    #shortcut_path = os.path.join(desktop, "detect_gui_alias")
+    target_path = os.path.join(directory, "detect_gui.command")
 
-    vbs_script = f"""
-Set WshShell = WScript.CreateObject("WScript.Shell")
-Set Shortcut = WshShell.CreateShortcut("{shortcut_path}")
-Shortcut.TargetPath = "{target_path}"
-Shortcut.WorkingDirectory = "{os.path.dirname(target_path)}"
-Shortcut.WindowStyle = 1
-Shortcut.IconLocation = "{target_path}, 0"
-Shortcut.Save
-    """
-    with open("create_shortcut.vbs", "w") as file:
-        file.write(vbs_script)
+    # AppleScript to create alias
+    applescript = """
+    tell application "Finder"
+        make alias file to POSIX file "{target_path}" at POSIX file "{desktop}"
+        set name of result to "detect_gui_alias"
+    end tell
+    """.format(target_path,desktop)
+    script_path = os.path.join(directory, "create_alias.scpt")
+    with open(script_path, "w") as file:
+        file.write(applescript)
 
-    subprocess.run(['cscript', '//nologo', 'create_shortcut.vbs'])
-    os.remove("create_shortcut.vbs")
-
+    subprocess.run(['osascript', script_path])
+    os.remove(script_path)
 
 if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
     create_shortcuts_in_directory(current_dir)
     print("Shortcuts created successfully on the desktop.")
