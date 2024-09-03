1386a556311dfc9105d7693c70ee788e1e685efd
Tkanno <38493521+yodaka0@users.noreply.github.com>
2024-09-03 16:46:16 +0900

    Update mdet_setup.py

--- mdet_setup.py
+++ mdet_setup.py
@@ -1,14 +1,20 @@
 import os
 import subprocess
 import zipfile
-import urllib.request
 import platform
+import urllib.request
+#import stat
+import shutil
 try:
     import requests
 except ImportError:
     subprocess.run("pip install requests", shell=True, check=True)
     import requests
 
+
+"""def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)"""
 
 def download_miniconda(installer_url):
     response = requests.get(installer_url, stream=True)
@@ -22,101 +28,105 @@ def download_miniconda(installer_url):
 
     return downloaded_installer_path
 
-def install_miniconda(installer_path):
-    base_installer_path = os.path.basename(installer_path)
-    if platform.system() == 'Windows':
-        subprocess.run([base_installer_path, '/S'], check=True)
-        print("Miniconda installed successfully")
-    else:
-        subprocess.run(['bash', base_installer_path, '-b', '-p', os.path.expanduser('~/miniconda')], check=True)
+def install_miniconda(downloaded_installer_path, abs_conda_path):
+    subprocess.run([downloaded_installer_path, '/S'], check=True)
+    os.remove(downloaded_installer_path)
+    run_command(f"{os.path.join(abs_conda_path, 'conda')} --version")
+    print("Miniconda installed successfully")
     
 
-def add_conda_to_path():
-    if platform.system() == 'Windows':
-        # Add Miniconda to PATH from 'C:\username\Miniconda\Scripts'
-        conda_path = os.path.expanduser('~/miniconda3/Scripts')
-        current_path = os.environ['PATH']
-        if conda_path not in current_path:
-            os.environ['PATH'] = f"{conda_path};{current_path}"
-            subprocess.run(['setx', 'PATH', os.environ['PATH']], check=True)
-            print(f"Added {conda_path} to PATH")
-    else:
-        conda_path = os.path.expanduser('~/miniconda/bin')
-        shell = os.environ.get('SHELL')
-        if shell and 'bash' in shell:
-            profile_path = os.path.expanduser('~/.bashrc')
-        elif shell and 'zsh' in shell:
-            profile_path = os.path.expanduser('~/.zshrc')
-        else:
-            profile_path = os.path.expanduser('~/.profile')
-        
-        with open(profile_path, 'a') as file:
-            file.write(f'\n# Added by Python script\nexport PATH="{conda_path}:$PATH"\n')
-        subprocess.run([shell, '-c', f'source {profile_path}'], check=True)
-        print(f"Added {conda_path} to PATH in {profile_path}")
+def add_conda_to_path(abs_conda_path):
 
+    current_path = os.environ['PATH']
+    if abs_conda_path not in current_path:
+        new_path = f"{abs_conda_path};{current_path}"
+        subprocess.run(['setx', 'PATH', new_path], check=True)
+        os.environ['PATH'] = new_path
+        print(f"Added {abs_conda_path} to PATH")
 
 def run_command(command):
     """Subprocess wrapper to run shell commands."""
     result = subprocess.run(command, shell=True, check=True, text=True)
     return result
 
-def clone_repository(repo_url,program_dir):
-    """Clone a Git repository."""
-    os.makedirs(os.path.expanduser(program_dir), exist_ok=True)
-    try:
-        # clone branch
-        run_command(f"git clone -b gui {repo_url} {program_dir}")
-    except subprocess.CalledProcessError:
-        run_command(f"git clone {repo_url} {program_dir}")
+def create_program_dir(project_dir, github_url):
+    repo_url = github_url + "ImageExtractWin.git"
+    zip_url = github_url + "ImageExtractWin/archive/refs/heads/gui.zip"
 
-def download_and_extract_zip(url, extract_to):
+    """Create a directory for the program."""
+    # If project directory already exists, check whether to overwrite
+    project_dir_path = os.path.expanduser(project_dir)
+    print("project directory is {}".format(project_dir_path))
+    if os.path.exists(project_dir_path):
+        print(f"{project_dir_path} already exists, do you want to overwrite it?")
+        overwrite = input("Overwrite? (y/n): ")
+        if overwrite.lower() == 'y':
+            try:
+                shutil.rmtree(project_dir_path)
+            except:
+                print(f"\033[32mFailed to remove existing directory. You should remove {project_dir_path} manually.\033[0m")
+                raise
+        else:
+            print("Exiting...")
+            return
+    
+    os.makedirs(project_dir_path, exist_ok=True)
+
+    try:
+        run_command(f"git clone -b gui {repo_url} {project_dir_path}")
+    except subprocess.CalledProcessError:
+        print("Git clone failed, attempting to download ZIP.")
+        download_and_extract_zip(zip_url, project_dir_path)
+
+    print("Repository cloned successfully into {}.".format(project_dir))
+
+def download_and_extract_zip(zip_url, project_dir):
     """Download and extract a ZIP file."""
-    zip_path = os.path.join(extract_to, "repo.zip")
-    urllib.request.urlretrieve(url, zip_path)
+    zip_path = "ImageExtractWin-gui.zip"
+    urllib.request.urlretrieve(zip_url, zip_path)
+    # Extract the ZIP file to the program directory
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_to)
+        zip_ref.extractall(project_dir)
     os.remove(zip_path)
 
 def main():
+    github_url = "https://github.com/yodaka0/"
+
+    if platform.system() == 'Windows':
+        # Change to the user's home directory
+        os.chdir(os.path.expanduser("~"))
+        print(os.getcwd())
+
+    else:
+        print("This script is only supported on Windows. If you are on macOS or Linux, please contact to {}.".format(github_url))
+        return
+
     # if conda hasn't been installed, install it
     try:
         run_command("conda --version")
         print("Conda is already installed")
     except subprocess.CalledProcessError:
         print("Conda is not installed, installing...")
-        if platform.system() == 'Windows':
-            installer_url = 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe'
-        elif platform.system() == 'Darwin':  # macOS
-            installer_url = 'https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh'
-        else:  # Linux
-            installer_url = 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh'
-
-        installer_path = download_miniconda(installer_url)
-        install_miniconda(installer_path)
-        add_conda_to_path()
-
-        if platform.system() != 'Windows':
-            conda_init_cmd = os.path.expanduser('~/miniconda/bin/conda init')
-            subprocess.run(['bash', '-c', conda_init_cmd], check=True)
-            print("Conda initialized successfully")
+        installer_url = 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe'
         
-        run_command("conda --version")
+        try:
+            downloaded_installer_path = download_miniconda(installer_url)
+            abs_conda_path = os.path.abspath("miniconda3/Scripts")
+            #print(abs_conda_path)
+            install_miniconda(downloaded_installer_path, abs_conda_path)
+            add_conda_to_path(abs_conda_path)
+        
+            run_command("conda --version")
+        except:
+            print(f"\033[32mFailed to install Miniconda. Please install Miniconda manually from {installer_url}\033[0m")
+            raise
 
-    repo_url = "https://github.com/yodaka0/ImageExtractWin.git"
-    zip_url = "https://github.com/yodaka0/ImageExtractWin/archive/refs/heads/gui.zip"
-    # project_dir = "C:\\Users\\username\\ImageExtractWin-gui"
+
     project_dir = "~/ImageExtractWin-gui"
 
-    # Clone the repository and change to the project directory
-    try:
-        clone_repository(repo_url, project_dir)
-    except subprocess.CalledProcessError:
-        print("Git clone failed, attempting to download ZIP.")
-        download_and_extract_zip(zip_url, os.path.expanduser("~"))
-        
+    create_program_dir(project_dir, github_url)
+
     os.chdir(os.path.expanduser(project_dir))
-    print(os.getcwd())
 
     # Run the make_batch.py script
     try:
