a13a49c8a879184e2f8fb8adc226395a94e2259b
Tkanno <38493521+yodaka0@users.noreply.github.com>
2024-11-18 08:37:09 +0900

    Update image_demo.py

--- image_demo.py
+++ image_demo.py
@@ -41,7 +41,7 @@ def pw_detect(im_file, new_file, threshold=None, pre_detects=None, diff_reasonin
             print("HerdNet model is too heavy for CPU, please use GPU")
             raise ValueError("Model not supported")
         elif DEVICE == "cuda":
-            detection_model = pw_detection.HerdNet(device=DEVICE)
+            detection_model = pw_detection.HerdNet(device=DEVICE, dataset="ennedi")
     else:
         raise ValueError("Model not supported")
 
@@ -62,33 +62,34 @@ def pw_detect(im_file, new_file, threshold=None, pre_detects=None, diff_reasonin
     # Performing the detection on the single image
     if model == "HerdNet":
         result = detection_model.single_image_detection(img=im_file)
+        #print(result)
     else:
         result = detection_model.single_image_detection(img=img, img_path=im_file, conf_thres=threshold)
 
     
-    #result['img_id'] = result['img_id'].replace("\\","/")
+        #result['img_id'] = result['img_id'].replace("\\","/")
 
-    if diff_reasoning and pre_detects is not None:
-        # extract values of bounding boxes from the result dictionary
-        bounding_boxes = result['detections'].xyxy
-        prev_bounding_boxes = pre_detects.xyxy
-        # if bounding boxes heardly change, then animal change into blank
-        if len(bounding_boxes) == len(prev_bounding_boxes):
-            for i in range(len(bounding_boxes)):
-                if (abs(bounding_boxes[i][0] - prev_bounding_boxes[i][0]) < 5 and
-                    abs(bounding_boxes[i][1] - prev_bounding_boxes[i][1]) < 5 and
-                    abs(bounding_boxes[i][2] - prev_bounding_boxes[i][2]) < 5 and
-                    abs(bounding_boxes[i][3] - prev_bounding_boxes[i][3]) < 5):
-                    print(f"bounding boxes:{bounding_boxes[i]}")
-                    print(f"previous bounding boxes:{prev_bounding_boxes[i]}")
-                    print("bounding box not move, change to blank")
-                    #transform animal in the labels to 'blank'
-                    result['labels'][i] = "blank"
+        if diff_reasoning and pre_detects is not None:
+            # extract values of bounding boxes from the result dictionary
+            bounding_boxes = result['detections'].xyxy
+            prev_bounding_boxes = pre_detects.xyxy
+            # if bounding boxes heardly change, then animal change into blank
+            if len(bounding_boxes) == len(prev_bounding_boxes):
+                for i in range(len(bounding_boxes)):
+                    if (abs(bounding_boxes[i][0] - prev_bounding_boxes[i][0]) < 5 and
+                        abs(bounding_boxes[i][1] - prev_bounding_boxes[i][1]) < 5 and
+                        abs(bounding_boxes[i][2] - prev_bounding_boxes[i][2]) < 5 and
+                        abs(bounding_boxes[i][3] - prev_bounding_boxes[i][3]) < 5):
+                        print(f"bounding boxes:{bounding_boxes[i]}")
+                        print(f"previous bounding boxes:{prev_bounding_boxes[i]}")
+                        print("bounding box not move, change to blank")
+                        #transform animal in the labels to 'blank'
+                        result['labels'][i] = "blank"
 
-    # Saving the detection results 
-    animal_n = sum('animal' in item for item in result['labels'])
-    print(f'{im_file} has {animal_n} animals')
-    result['object'] = animal_n
+        # Saving the detection results 
+        animal_n = sum('animal' in item for item in result['labels'])
+        print(f'{im_file} has {animal_n} animals')
+        result['object'] = animal_n
 
     try:
         img = Image.open(im_file)
@@ -101,22 +102,21 @@ def pw_detect(im_file, new_file, threshold=None, pre_detects=None, diff_reasonin
         result['eventEnd'] = "None"
         result["Make"] = None
 
-    if animal_n > 0:
+    if model == "HerdNet":
+        pw_utils.save_detection_images_dots(result, new_file_path, overwrite=False)
+    elif animal_n > 0:
         if verbose:
             print(f"Saving detection images to {new_file_path}")
             print(result)
-        if model == "HerdNet":
-            pw_utils.save_detection_images_dots(result, new_file_path, overwrite=False)
-        else:
-            pw_utils.save_detection_images(result, new_file_path, overwrite=False)
-            if classify:
-                try:
-                    animalclass = Classifier(model_dir=os.getcwd())
-                    sp, conf = animalclass.run_prediction(result, img)
-                    result['scientific_name'] = sp
-                    result['sp_confidence'] = conf
-                except Exception as e:
-                    print(f"Error in classification: {e}")
-                    raise
+        pw_utils.save_detection_images(result, new_file_path, overwrite=False)
+        if classify:
+            try:
+                animalclass = Classifier(model_dir=os.getcwd())
+                sp, conf = animalclass.run_prediction(result, img)
+                result['scientific_name'] = sp
+                result['sp_confidence'] = conf
+            except Exception as e:
+                print(f"Error in classification: {e}")
+                raise
 
     return result
